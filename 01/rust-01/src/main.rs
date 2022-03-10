54902e85ec2d079abbcb2997891db057a01082b6
Spencer Witt <spencer.witt@gmail.com>
2022-03-10 10:12:57 -0600

    implement solution for day 1 part 2

--- 01/rust-01/src/main.rs
+++ 01/rust-01/src/main.rs
@@ -10,14 +10,30 @@ fn main() {
     let content = fs::read_to_string(input).expect("Failed to read file");
     let readings: Vec<i32> = content
         .split_ascii_whitespace()
-        .map(|r| r.parse::<i32>().unwrap())
+        .map(|r| r.parse::<i32>().unwrap()) // this assumes the file contains valid input
         .collect();
     
+    let single_increases = calc_increases(&readings);
+    println!("There were {} single reading increases", single_increases);
+
+    let window_sums = sum_windows(&readings);
+    let window_increases = calc_increases(&window_sums);
+    println!("There were {} sliding window increases", window_increases);
+}
+
+fn sum_windows(readings: &[i32]) -> Vec<i32> {
+    readings
+        .windows(3)
+        .map(|w| w.iter().sum())
+        .collect()
+}
+
+fn calc_increases(readings: &[i32]) -> i32 {
     let mut increases = 0;
     for r in readings.windows(2) {
         if r[1] > r[0] {
             increases += 1;
         }
     }
-    println!("There were {} reading increases", increases);
+    return increases
 }
