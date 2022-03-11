47c37e23fce465fb17a038cc69d5b73feda6515a
Spencer Witt <spencer.witt@gmail.com>
2022-03-11 16:32:55 -0600

    implement solution for day 3 part 2

--- 03/notes.md
+++ 03/notes.md
@@ -4,3 +4,18 @@ This [puzzle](https://adventofcode.com/2021/day/3) requires parsing a list of bi
 Is it easier to manipulate and query the binary values as strings or by converting them to numeric values? Working with strings means a comparison for character value of '0' or '1'. Working with numeric values means bitwise operations and shifting.
 
 Treating the values as numbers seems more interesting, so I'll go with that option.
+
+## Part 2
+To get the required values for the second part, we need to get values for "oxygen generator rating" and "CO2 scrubber rating."
+
+In both cases we start with the most significant bit and find which bit is the most common. Oxygen is filtered to values with the _more common_ value in that position (or where value is `1` in case of a tie) while CO2 is filtered to values with the _less common_ value in that position (or where value is `0` in case of a tie).
+
+The process is repeated until there is just one value left for each rating. This may occur before all bits are processed.
+
+Multiply the two ratings together to get the answer to the puzzle.
+
+Unfortunately my code for the first part does not play so nicely with this second part. It may be easier to change to using the strings here, but I will try to keep using the numeric values. I'll start by refactoring my code to get a _count_ of the values in a vector that match a bitmask. This is kind of an inversion of what I was doing before: rather than loop over values and applying each bitmask to each value, I will apply a single bitmask to _every_ value in a single pass. This requires multiple iterations over the values, but the total number of iterations between input values and bitmask values will not change.
+
+My first thought was to partition the remaining set each time, but that only works for checking the first bit. After the first bit is processed, the two sets of values we operate on are changed.
+
+The first pass at the solution did not yield the expected result. I used the sample data given in the puzzle description for further testing and realized that I had issues with integer division. When comparing whether the count of 1s to check whether it was more or less than half the remaining values, I was performing integer division on the length of the vector. This was sometimes causing the program to keep the opposite of the values that it should for the next iteration.
--- 03/rust-03/src/main.rs
+++ 03/rust-03/src/main.rs
@@ -1,7 +1,8 @@
+use std::env;
 use rust_aoc;
 
 fn main() {
-    let input_lines = rust_aoc::get_default_input_lines();
+    let input_lines = rust_aoc::get_default_input_lines(Some(env::args().collect()));
 
     let line_length = input_lines[0].len();
     // check that all lines have the same length
@@ -9,15 +10,17 @@ fn main() {
 
     let input_values: Vec<usize> = input_lines.iter().map(|l| usize::from_str_radix(l, 2).unwrap()).collect();
     p1(&input_values, line_length);
+    p2(&input_values, line_length);
 }
 
+/// Get the count of items in `values` that match `bitmask`
 fn get_count_match_bitmask(values: &[usize], bitmask: usize) -> usize {
-    values.iter().filter(|&v| v & bitmask > 0).count()
+    values.iter().filter(|&v| v & bitmask != 0).count()
 }
 
 fn p1(values: &[usize], line_length: usize) {
     let mut ones_count = Vec::<usize>::new();
-    let mut bitmask = 1;
+    let mut bitmask: usize = 1;
     for _ in 0..line_length {
         ones_count.push(get_count_match_bitmask(values, bitmask));
         bitmask = bitmask << 1;
@@ -47,4 +50,61 @@ fn p1(values: &[usize], line_length: usize) {
     println!("Gamma rate: {}", gamma_rate);
     println!("Epsilon rate: {}", epsilon_rate);
     println!("Power consumption: {}", gamma_rate * epsilon_rate);
+}
+
+fn p2(values: &[usize], line_length: usize) {
+    let oxy_rating = find_oxy_rating(&values, line_length);
+    let co2_rating = find_co2_rating(&values, line_length);
+    println!("--- Part II ---");
+    println!("Oxygen generator rating: {}", oxy_rating);
+    println!("CO2 scrubber rating: {}", co2_rating);
+    println!("Life support rating: {}", oxy_rating * co2_rating);
+}
+
+fn find_oxy_rating(values: &[usize], line_length: usize) -> usize {
+    let mut bitmask: usize = 1 << (line_length - 1);
+    let mut oxy_values = values.to_vec();
+    for _ in 0..line_length {
+        let ones_count = get_count_match_bitmask(&oxy_values, bitmask);
+        let ones_count_cutoff = oxy_values.len() / 2 + oxy_values.len() % 2;
+        if ones_count >= ones_count_cutoff {
+            // ones are more (or equally) common, filter to values matching bitmask
+            oxy_values = oxy_values.into_iter().filter(|v| v & bitmask != 0).collect();
+        } else {
+            // otherwise we filter to values not matching bitmask
+            oxy_values = oxy_values.into_iter().filter(|v| v & bitmask == 0).collect();
+        }
+        if oxy_values.len() == 1 {
+            // exit early if just one value remains
+            break;
+        }
+        bitmask = bitmask >> 1;
+    }
+    // make sure we have only one value left
+    assert_eq!(oxy_values.len(), 1);
+    return oxy_values[0];
+}
+
+fn find_co2_rating(values: &[usize], line_length: usize) -> usize {
+    let mut bitmask: usize = 1 << (line_length - 1);
+    let mut co2_values = values.to_vec();
+    for _ in 0..line_length {
+        let ones_count = get_count_match_bitmask(&co2_values, bitmask);
+        let ones_count_cutoff = co2_values.len() / 2 + co2_values.len() % 2;
+        if ones_count >= ones_count_cutoff {
+            // ones are more (or equally) common, filter to values not matching bitmask
+            co2_values = co2_values.into_iter().filter(|v| v & bitmask == 0).collect();
+        } else {
+            // otherwise we filter to values matching bitmask
+            co2_values = co2_values.into_iter().filter(|v| v & bitmask != 0).collect();
+        }
+        if co2_values.len() == 1 {
+            // exit early if just one value remains
+            break;
+        }
+        bitmask = bitmask >> 1;
+    }
+    // make sure we have only one value left
+    assert_eq!(co2_values.len(), 1);
+    return co2_values[0];
 }
